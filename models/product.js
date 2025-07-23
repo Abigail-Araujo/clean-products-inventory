9d631d770778e32793da16f3f3bf0a3db4984b15
Abigail Araujo <abi07052004@gmail.com>
2025-07-23 14:26:21 -0400

    Fix lowercase stock

--- models/product.js
+++ models/product.js
@@ -27,7 +27,6 @@ const productSchema = new mongoose.Schema({
 productSchema.pre("save", function (next) {
   if (this.name) this.name = this.name.toLowerCase();
   if (this.description) this.description = this.description.toLowerCase();
-  if (this.stock) this.stock = this.stock;
   next();
 });
 
